4162652596fcbfa32ffde1aec8698baad3e37ce5
leesumin <lsumin1127@gmail.com>
2021-12-20 12:41:36 +0900

    2021-12-20 12:41pm LEE

--- src/main/java/com/bookshop/service/BoardService.java
+++ src/main/java/com/bookshop/service/BoardService.java
@@ -1,5 +1,12 @@
 package com.bookshop.service;
 
+import com.bookshop.vo.Book;
+
 public interface BoardService {
 
+	
+	// 도서정보 불러오기 service
+	public Book getBookInfo(String book_id) throws Exception;
+	
+	
 }
