d346cd2dd4c4d1c5836000cdf14ca8f9a925629e
CHOIJXWXN <pinkytime96@gmail.com>
2021-12-20 19:33:35 +0900

    2021-12-20 CHOI

--- src/main/java/com/bookshop/service/OrderService.java
+++ src/main/java/com/bookshop/service/OrderService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.bookshop.vo.Cart;
 import com.bookshop.vo.CartPlus;
+import com.bookshop.vo.Orders;
 
 public interface OrderService {
 	
@@ -13,5 +14,8 @@ public interface OrderService {
 	public void deleteCart(Cart cart) throws Exception;
 	public void updateCart(Cart cart) throws Exception;
 	public String newOrderNum() throws Exception;
-
+	public int addOrder(Orders order) throws Exception;
+	public void point(String user_id, int point_use, int point_add) throws Exception;
+	public void sellTot(Cart cart) throws Exception;
+	
 }
