5f2df93555b1c2199683de9f1ff66bc9e65cacfa
Jayyouu <duwls5513@naver.com>
2021-12-23 09:52:55 +0900

    문의 list 이미지 수정

--- src/main/java/com/bookshop/vo/AskList.java
+++ src/main/java/com/bookshop/vo/AskList.java
@@ -6,39 +6,62 @@ public class AskList {
 	private String book_title;
 	private String ask_sort;
 	private String ask_date;
+	private String book_cover;
 	
 	public AskList() {}
-	
-	public AskList(int ask_id, String book_title, String ask_sort, String ask_date) {
+
+	public AskList(int ask_id, String book_title, String ask_sort, String ask_date, String book_cover) {
 		this.ask_id = ask_id;
 		this.book_title = book_title;
 		this.ask_sort = ask_sort;
 		this.ask_date = ask_date;
+		this.book_cover = book_cover;
 	}
+
 	public int getAsk_id() {
 		return ask_id;
 	}
+
 	public void setAsk_id(int ask_id) {
 		this.ask_id = ask_id;
 	}
+
 	public String getBook_title() {
 		return book_title;
 	}
+
 	public void setBook_title(String book_title) {
 		this.book_title = book_title;
 	}
+
 	public String getAsk_sort() {
 		return ask_sort;
 	}
+
 	public void setAsk_sort(String ask_sort) {
 		this.ask_sort = ask_sort;
 	}
+
 	public String getAsk_date() {
 		return ask_date;
 	}
+
 	public void setAsk_date(String ask_date) {
 		this.ask_date = ask_date;
 	}
+
+	public String getBook_cover() {
+		return book_cover;
+	}
+
+	public void setBook_cover(String book_cover) {
+		this.book_cover = book_cover;
+	}
+	
+	
+	
+
+	
 	
 	
 	
