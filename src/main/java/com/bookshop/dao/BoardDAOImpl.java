f69b94fcec085ccc016b9b4ff220940b84f0b01f
Jayyouu <duwls5513@naver.com>
2021-12-24 02:40:41 +0900

    AskReply insert delete ok 1

--- src/main/java/com/bookshop/dao/BoardDAOImpl.java
+++ src/main/java/com/bookshop/dao/BoardDAOImpl.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Repository;
 
 import com.bookshop.vo.Ask;
 import com.bookshop.vo.AskList;
+import com.bookshop.vo.AskReply;
 import com.bookshop.vo.Book;
 
 @Repository
@@ -23,6 +24,12 @@ public class BoardDAOImpl implements BoardDAO {
 	@Override
 	public Book getBookInfo(String book_id) throws Exception {
 		return sqlSession.selectOne(SESSION + ".getBookInfo", book_id);
+	}
+	
+	// ASK table에서 Book_id 불러오기
+	@Override
+	public String getBookid(int ask_id) throws Exception {
+		return sqlSession.selectOne(SESSION + ".getBookid", ask_id);
 	}
 	
 	// 목록 불러오기
@@ -44,5 +51,24 @@ public class BoardDAOImpl implements BoardDAO {
 		return sqlSession.selectOne(SESSION + ".getAsk", ask_id);
 	}
 	
+	// 댓글 작성
+	@Override
+	public void insertAskReply(AskReply askreply) throws Exception {
+		sqlSession.insert(SESSION + ".insertAskReply", askreply);
+	}
+	
+	// 댓글 불러오기
+	@Override
+	public List<AskReply> getAskReplyList(int ask_id) throws Exception {
+		return sqlSession.selectList(SESSION + ".getAskReply", ask_id);
+	}
+	
+	// 댓글 삭제
+	@Override
+	public void deleteAskReply(int askreply_id) throws Exception {
+		sqlSession.delete(SESSION + ".deleteAskReply", askreply_id);
+	}
+	
+
 
 }
--- src/main/java/com/bookshop/vo/AskReply.java
+++ src/main/java/com/bookshop/vo/AskReply.java
@@ -1,5 +1,63 @@
 package com.bookshop.vo;
 
 public class AskReply {
+	
+	private int askreply_id;			
+	private int ask_id;				
+	private String writer;				
+	private String askreply_contents;	
+	private String askreply_date;
+	
+	public AskReply() {}
+
+	public AskReply(int askreply_id, int ask_id, String writer, String askreply_contents, String askreply_date) {
+		this.askreply_id = askreply_id;
+		this.ask_id = ask_id;
+		this.writer = writer;
+		this.askreply_contents = askreply_contents;
+		this.askreply_date = askreply_date;
+	}
+
+	public int getAskreply_id() {
+		return askreply_id;
+	}
+
+	public void setAskreply_id(int askreply_id) {
+		this.askreply_id = askreply_id;
+	}
+
+	public int getAsk_id() {
+		return ask_id;
+	}
+
+	public void setAsk_id(int ask_id) {
+		this.ask_id = ask_id;
+	}
+
+	public String getWriter() {
+		return writer;
+	}
+
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+
+	public String getAskreply_contents() {
+		return askreply_contents;
+	}
+
+	public void setAskreply_contents(String askreply_contents) {
+		this.askreply_contents = askreply_contents;
+	}
+
+	public String getAskreply_date() {
+		return askreply_date;
+	}
+
+	public void setAskreply_date(String askreply_date) {
+		this.askreply_date = askreply_date;
+	}
+	
+
 
 }
