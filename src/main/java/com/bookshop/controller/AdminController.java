5d778cdcd56902ade296c6651479c795baa66c62
CHOIJXWXN <82311639+CHOIJXWXN@users.noreply.github.com>
2021-12-28 14:36:42 +0900

    Update AdminController.java

--- src/main/java/com/bookshop/controller/AdminController.java
+++ src/main/java/com/bookshop/controller/AdminController.java
@@ -51,7 +51,7 @@ public class AdminController {
 	}
 	
 	// 배송상태 변경 기능
-	@RequestMapping(value = "/changeStatus", method = RequestMethod.GET)
+	@RequestMapping(params = "status", value = "/changeStatus", method = RequestMethod.GET)
 	public String start(@RequestParam List<String> order_num, String status, HttpServletRequest request, Model model) throws Exception {
 		// 각 주문번호마다 배송상태 변경
 		for (var i = 0; i < order_num.size(); i++) {
